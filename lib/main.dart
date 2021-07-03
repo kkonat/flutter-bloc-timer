e5c1aa198e99659ed9039032ce03f6cbb47a2238
kkonat <presto@presto.biz.pl>
2021-07-03 11:19:36 +0200

    gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Bloc counter',
         theme: ThemeData(
-          primarySwatch: Colors.yellow,
+          primarySwatch: Colors.indigo,
         ),
         home: MyHomePage(title: 'Bloc counter'),
       ),
@@ -33,62 +33,81 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if (state.counterValue == 0) {
-            FlutterRingtonePlayer.playNotification();
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text('Finished!'),
-              duration: Duration(seconds: 5),
-            ));
-          }
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Set time then press play to count down',
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FloatingActionButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).decrement();
-                      },
-                      tooltip: 'Decrease',
-                      child: Icon(Icons.remove)),
-                  BlocBuilder<CounterCubit, CounterState>(
-                    builder: (context, state) {
-                      return Text(
-                        state.counterValue.toString(),
-                        style: Theme.of(context).textTheme.headline4,
-                      );
-                    },
-                  ),
-                  FloatingActionButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).increment();
-                      },
-                      tooltip: 'Increase',
-                      child: Icon(Icons.add)),
-                ],
-              ),
-              FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).starttimer();
-                  },
-                  tooltip: 'Start',
-                  child: Icon(Icons.arrow_back_rounded)),
+      body: Container(
+        decoration: BoxDecoration(
+          // Box decoration takes a gradient
+          gradient: LinearGradient(
+            // Where the linear gradient begins and ends
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            // Add one stop for each color. Stops should increase from 0 to 1
+            stops: [0.1, 0.5, 0.7, 0.9],
+            colors: [
+              // Colors are easy thanks to Flutter's Colors class.
+              Color(0xFF42A5F5),
+              Color(0xFFBBDEFB),
+              Color(0xFF9FA8DA),
+              Color(0xFF00ACC1),
             ],
+          ),
+        ),
+        child: BlocListener<CounterCubit, CounterState>(
+          listener: (context, state) {
+            if (state.counterValue == 0) {
+              FlutterRingtonePlayer.playNotification();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('Finished!'),
+                duration: Duration(seconds: 5),
+              ));
+            }
+          },
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Set time then press play to count down',
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FloatingActionButton(
+                        onPressed: () {
+                          BlocProvider.of<CounterCubit>(context).decrement();
+                        },
+                        tooltip: 'Decrease',
+                        child: Icon(Icons.remove)),
+                    BlocBuilder<CounterCubit, CounterState>(
+                      builder: (context, state) {
+                        return Text(
+                          state.counterValue.toString(),
+                          style: Theme.of(context).textTheme.headline4,
+                        );
+                      },
+                    ),
+                    FloatingActionButton(
+                        onPressed: () {
+                          BlocProvider.of<CounterCubit>(context).increment();
+                        },
+                        tooltip: 'Increase',
+                        child: Icon(Icons.add)),
+                  ],
+                ),
+                FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).starttimer();
+                    },
+                    tooltip: 'Start',
+                    child: Icon(Icons.arrow_back_rounded)),
+              ],
+            ),
           ),
         ),
       ),
